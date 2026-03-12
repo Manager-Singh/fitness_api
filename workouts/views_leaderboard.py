f85b71466769119d6707c9572ee4a67f17f20f78
Pankaj <pankaj@localhost.localdomain>
2026-03-12 15:44:28 +0530

    new key add in response

--- workouts/views_leaderboard.py
+++ workouts/views_leaderboard.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
+from django.utils import timezone
+from datetime import timedelta
 
 from .serializers_leaderboard import LeaderboardEntrySerializer
 
